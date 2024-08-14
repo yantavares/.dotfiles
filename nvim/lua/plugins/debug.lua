0734106d16f66580178d98402837e59169a4ca30
yantavares <yantdo1@gmail.com>
2024-08-14 12:01:48 -0300

    added dap for debugging

---
+++ nvim/lua/plugins/debug.lua
@@ -0,0 +1,25 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local dap = require("dap")
+
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end)
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end)
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end)
+	end,
+}
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,9 +15,13 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
+            lspconfig.html.setup({capabilities = capabilities})
+            lspconfig.lua_ls.setup({{capabilities = capabilities}})
+            lspconfig.tsserver.setup({{capabilities = capabilities}})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
