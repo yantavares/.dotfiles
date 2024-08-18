88c32d5798058fb290d2d39f5793468e9e353967
yantavares <yantdo1@gmail.com>
2024-08-18 10:05:32 -0300

    added emmet support

--- nvim/lua/custom/buffers.lua
+++ nvim/lua/custom/buffers.lua
@@ -1,41 +1,42 @@
 local buffer_history = {}
 
-local update_buffer_history = function ()
-  local current_buf = vim.api.nvim_get_current_buf()
-  local buf_name = vim.api.nvim_buf_get_name(current_buf)
+local update_buffer_history = function()
+    local current_buf = vim.api.nvim_get_current_buf()
+    local buf_name = vim.api.nvim_buf_get_name(current_buf)
+    local buf_type = vim.api.nvim_buf_get_option(current_buf, "buftype")
 
-  -- Ensure the buffer is listed and has a valid name
-  if not vim.api.nvim_buf_get_option(current_buf, "buflisted") or buf_name == "" then
-    return
-  end
-
-  -- Remove the current buffer if it exists in the history
-  for i, buf in ipairs(buffer_history) do
-    if buf == current_buf then
-      table.remove(buffer_history, i)
-      break
+    -- Ensure the buffer is listed, has a valid name, and is not a terminal buffer
+    if not vim.api.nvim_buf_get_option(current_buf, "buflisted") or buf_name == "" or buf_type == "terminal" then
+        return
     end
-  end
 
-  table.insert(buffer_history, 1, current_buf)
+    -- Remove the current buffer if it exists in the history
+    for i, buf in ipairs(buffer_history) do
+        if buf == current_buf then
+            table.remove(buffer_history, i)
+            break
+        end
+    end
 
-  if #buffer_history > 2 then
-    table.remove(buffer_history, 3)
-  end
+    table.insert(buffer_history, 1, current_buf)
+
+    if #buffer_history > 2 then
+        table.remove(buffer_history, 3)
+    end
 end
 
 local toggle_buffers = function()
-  if #buffer_history < 2 then
-    print("No other buffer to switch to")
-    return
-  end
-  local current_buf = vim.api.nvim_get_current_buf()
-  for _, buf in ipairs(buffer_history) do
-    if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, "buflisted") then
-      vim.api.nvim_set_current_buf(buf)
-      return
+    if #buffer_history < 2 then
+        print("No other buffer to switch to")
+        return
     end
-  end
+    local current_buf = vim.api.nvim_get_current_buf()
+    for _, buf in ipairs(buffer_history) do
+        if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, "buflisted") then
+            vim.api.nvim_set_current_buf(buf)
+            return
+        end
+    end
 end
 
 -- Autocommand to update buffer history on buffer switch
@@ -46,10 +47,14 @@ vim.cmd([[
   augroup END
 ]])
 
-vim.api.nvim_set_keymap('n', '<leader><tab>', ':lua require("custom.buffers").toggle_buffers()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader><tab>",
+    ':lua require("custom.buffers").toggle_buffers()<CR>',
+    { noremap = true, silent = true }
+)
 
 return {
     toggle_buffers = toggle_buffers,
-    update_buffer_history = update_buffer_history
+    update_buffer_history = update_buffer_history,
 }
-
